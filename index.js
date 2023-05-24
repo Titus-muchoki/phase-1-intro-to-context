882ff2bd76ab9a49b4ea7142ad66fcb4111d3b6c
kajela <titoyut56@gmail.com>
2023-05-24 02:29:07 -0400

    add the initial files

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function createEmployeeRecord(array){
+    const obj = {
+firstName: [0],
+familyName: array[1],
+title: [2],
+payPerHour: [3],
+timeInEvents: [],
+timeOutEvents: []
+    }
+    return obj;
+}
